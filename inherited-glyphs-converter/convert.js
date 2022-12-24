774ea73b736af9a6c53b2d46796e7effcf22f8fa
haydenwong7bm <51369959+haydenwong7bm@users.noreply.github.com>
2022-12-24 22:57:26 +0800

    Added options

--- inherited-glyphs-converter/convert.js
+++ inherited-glyphs-converter/convert.js
@@ -1,5 +1,7 @@
 function convert() {
-	fetch("https://raw.githubusercontent.com/haydenwong7bm/inherited-glyphs-converter/main/conversion-tables/variants_list.txt")
+	const SOURCE_URL = "https://raw.githubusercontent.com/haydenwong7bm/inherited-glyphs-converter/main/conversion-tables/variants_list.txt"
+	
+	fetch(SOURCE_URL)
 	.then(response => response.text())
 	.then(textString => {
 		var table = textString.trim().split("\n");
@@ -7,9 +9,27 @@ function convert() {
 		
 		var i = 0;
 		var string = document.getElementById('input').value;
+		var options = document.getElementById('supp').value;
+		
+		if (document.getElementById('j').checked) {
+			options += "j";
+		}
+		if (document.getElementById('k').checked) {
+			options += "k";
+		}
+		if (document.getElementById('t').checked) {
+			options += "t";
+		}
+		if (document.getElementById('i').checked) {
+			options += "i";
+		}
+		
+		options = new RegExp("[" + options + "]");
 		
 		while (i < table.length) {
-			string = string.replace(table[i][0], table[i][1]);
+			if ((table[i].length == 2) || (table[i].length == 3 && table[i][2].match(options))) {
+				string = string.replace(table[i][0], table[i][1]);
+				}
 			i++;
 		}
 		
