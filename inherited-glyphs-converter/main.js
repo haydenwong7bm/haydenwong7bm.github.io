bf10647cbee218e54d5103169e16224e8c8466fa
haydenwong7bm <51369959+haydenwong7bm@users.noreply.github.com>
2022-12-25 03:35:14 +0800

    Updated

---
+++ inherited-glyphs-converter/main.js
@@ -0,0 +1,56 @@
+function convert() {
+	const SOURCE_URL = ["https://raw.githubusercontent.com/haydenwong7bm/inherited-glyphs-converter/main/conversion-tables/variants_list.txt", "https://raw.githubusercontent.com/haydenwong7bm/inherited-glyphs-converter/main/conversion-tables/radicals.txt"]
+	
+	var supp_option = new RegExp(document.getElementById('supp').value);
+	
+	var var_options = '';
+	
+	if (document.getElementById('j').checked) {
+		var_options += "j";
+	}
+	if (document.getElementById('k').checked) {
+		var_options += "k";
+	}
+	if (document.getElementById('t').checked) {
+		var_options += "t";
+	}
+	if (document.getElementById('i').checked) {
+		var_options += "i";
+	}
+	
+	var_options = new RegExp("[" + var_options + "]");
+	
+	var text_input = document.getElementById('input');
+	text_input = text_input.value;
+	
+	for (let k = 0; k < SOURCE_URL.length; k++) {
+		fetch(SOURCE_URL[k])
+		.then(response => response.text())
+		.then(table_text => {
+			var table = table_text.trim().split("\n").map(x => x.split("\t"));
+			
+			for (let i = 0; i < table.length; i++) {
+				if (table[i].length == 2) {
+					text_input = text_input.replace(table[i][0], table[i][1]);
+				} else {
+				if (table[i].length == 3) {
+					if (table[i][2].match(var_options)) {
+						if (table[i][2].match(/[c*]/)) {
+							if (table[i][2].match(supp_option)) {
+								text_input = text_input.replace(table[i][0], table[i][1]);
+							}
+						} else {
+							text_input = text_input.replace(table[i][0], table[i][1]);
+						}
+					}
+				}
+				}
+			}
+			document.getElementById('output').value = text_input;
+		})
+	}
+}
+
+function copy() {
+	navigator.clipboard.writeText(document.getElementById('output').value)
+}
